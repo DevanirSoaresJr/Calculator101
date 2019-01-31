915113f7903a97c087be20d6622dac598617c5c4
Devanir Jr <devanirsoaresjr@gmail.com>
2019-01-31 12:21:53 +0000

    Created Buttons, Add Button Has some implementation and some what validation for empty fields

--- app/src/main/java/devanir/soaresjunior/calculator/MainActivity.java
+++ app/src/main/java/devanir/soaresjunior/calculator/MainActivity.java
@@ -2,8 +2,22 @@ package devanir.soaresjunior.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNumberOne;
+    EditText etNumberTwo;
+    Button btnAdd;
+    Button btnSubtract;
+    Button btnDivide;
+    Button btnMultiply;
+    TextView tvResults;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +25,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String message = "Hello";
+
+        etNumberOne = findViewById(R.id.etNumber1);
+        etNumberTwo = findViewById(R.id.etNumber2);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        tvResults = findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String result = add(etNumberOne.getText().toString(),
+                etNumberTwo.getText().toString());
+
+            tvResults.setText(result);
+            }
+        });
+
+    }
+
+    private String add(String numberOne, String numberTwo){
+        if(numberOne.equals("")|| numberTwo.isEmpty()) {
+            Toast.makeText(this,
+                    "Please fill the empty gaps with valid numbers pretty pleeaaaaase",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        int a = Integer.parseInt(numberOne);
+        int b = Integer.parseInt(numberTwo);
+        int result = a + b;
+        /*return Integer.toString(result);*/
+        return String.valueOf(result);
     }
 }
